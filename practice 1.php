0902e862f0635d76be6b9f76c9a90a41d81d6c8d
farhana2000ahmad <92678509+farhana2000ahmad@users.noreply.github.com>
2022-01-18 09:39:20 +0600

    practice file

---
+++ practice 1.php
@@ -0,0 +1,14 @@
+<?php
+$name="Farhana";
+$surname="Ahmad";
+$x=10;
+$y=18;
+$z=$x**$y;
+echo $name." ".$surname;
+echo "<br>";
+echo strlen($name);
+echo "<br>";
+echo strrev($name);
+echo "<br>";
+echo "The total of this number is :", $z;
+?>
---
+++ practice 2.php
@@ -0,0 +1,8 @@
+<?php
+echo (abs(-1.7));
+echo "<br>";
+echo (round(0.2));
+echo "<br>";
+echo (rand(0, 10));
+
+?>
